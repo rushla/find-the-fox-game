b46ea14f92e80d3000d084a84d0a51cfb827b3c0
rushla <rushla.castro@hotmail.com>
2019-06-02 22:12:47 -0300

    initial commit

---
+++ scripts/mainScript.js
@@ -0,0 +1,58 @@
+var foxElement = document.getElementById("fox");
+var buttonStartElement = document.getElementById("button-start")
+var width = innerWidth;
+var height = innerHeight;
+var foxWidth = 90;
+var foxHeight = 110;
+
+
+
+var interval = 1500;
+var won = false;
+function init(){
+    randomPlacingFox();
+    playAudio();
+    document.onmousemove = mouseMoved;
+    buttonStartElement.style.display = "none";
+}
+function playAudio(){    
+    var audio = new Audio("./assets/sounds/fox.mp3");
+    if(!won)
+        audio.play();
+    setTimeout(function(){
+        playAudio()
+    },interval);
+}
+function randomPlacingFox(){
+    var foxPositionX = Math.random() * (width - foxWidth);
+    if(foxPositionX < foxWidth){
+        foxPositionX = 0;
+    }
+    foxElement.style.marginLeft = foxPositionX + "px";
+    var foxPositionY = Math.random() * (height - foxHeight);
+    if(foxPositionY < foxHeight){
+        foxPositionY = 0;
+    }
+    foxElement.style.marginTop = foxPositionY +"px";
+    foxElement.style.display = "block";
+    foxElement.style.opacity = 0;
+}
+function mouseMoved(mouseEvent){
+    
+    var mousePositionX = mouseEvent.clientX;
+    var mousePositionY = mouseEvent.clientY;
+    var centroFoxX = Number(foxElement.style.marginLeft.split("px")[0]) + foxWidth / 2;
+    var centroFoxY = Number(foxElement.style.marginTop.split("px")[0]) + foxHeight / 2;
+    var distance = Math.sqrt((mousePositionX - centroFoxX)**2 + (mousePositionY - centroFoxY)**2);
+    interval = (700/width) * distance + 300;
+
+    
+}
+function win(){
+    foxElement.style.opacity = 1;
+    var whatDoesTheFoxSayAudio = new Audio('./assets/sounds/what_does_the_foxxx.mp3');
+    whatDoesTheFoxSayAudio.play();
+    won = true;
+    foxElement.classList.add("animate-fox");
+    //alert("What does the fox say?")
+}
